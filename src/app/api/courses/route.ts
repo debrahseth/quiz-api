a4fb0036dd83f819c9168289688ecf71d02f375e
debrahseth <debrahseth437@gmail.com>
2025-09-22 18:53:18 +0000

    Hope

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -1,10 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import coursesData from "../../../data/courses.json";
 
-export async function GET(
-  request: NextRequest,
-  context: { params?: { course: string } }
-) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const program = searchParams.get("program");
   const level = searchParams.get("level");
